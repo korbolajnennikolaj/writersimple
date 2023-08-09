837eb51c19f921ff294e8fecb52b68bc05810424
korbolajnennikolaj <141848103+korbolajnennikolaj@users.noreply.github.com>
2023-08-09 19:10:48 +0300

    Create example.py
    list of commands of module writersimple python

---
+++ example.py
@@ -0,0 +1,14 @@
+import writersimple
+
+writer.write("hello", 4, 5) 
+writer.write_revers("hello", 4, 5)
+writer.write_combo("hello", 4, 5)
+
+
+
+
+
+
+#    ||------------------------------
+#    \/                             |
+#writer.writer_cool(1, 1)    # :) look at this 
