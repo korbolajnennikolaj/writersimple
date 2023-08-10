5346efdb5aabf8a0ada9bda834641264dc651c98
korbolajnennikolaj <141848103+korbolajnennikolaj@users.noreply.github.com>
2023-08-10 13:30:51 +0300

    Create example_v_1.4.0.py

---
+++ example_v_1.4.0.py
@@ -0,0 +1,15 @@
+import writersimple
+
+print(writersimple.write("hello", 4, 5))
+print(writersimple.write_revers("hello", 4, 5))
+print(writersimple.write_combo("hello", 4, 5))
+
+
+
+
+
+
+#    ||---------------------------------------
+#    \/                                       |
+#print(writersimple.writer_cool(1, 1))    # :) look at this 
+
