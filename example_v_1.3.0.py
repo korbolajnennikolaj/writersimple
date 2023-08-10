7d088a3bbdc90654ad9de02b9d5b88905cd4fca3
korbolajnennikolaj <141848103+korbolajnennikolaj@users.noreply.github.com>
2023-08-10 13:31:33 +0300

    example_v_1.3.0.py

--- example_v_1.3.0.py
+++ example_v_1.3.0.py
@@ -1,8 +1,8 @@
 import writersimple
 
-writer.write("hello", 4, 5) 
-writer.write_revers("hello", 4, 5)
-writer.write_combo("hello", 4, 5)
+writersimple.write("hello", 4, 5) 
+writersimple.write_revers("hello", 4, 5)
+writersimple.write_combo("hello", 4, 5)
 
 
 
@@ -11,4 +11,4 @@ writer.write_combo("hello", 4, 5)
 
 #    ||------------------------------
 #    \/                             |
-#writer.writer_cool(1, 1)    # :) look at this 
+#writersimple.writer_cool(1, 1)    # :) look at this 
