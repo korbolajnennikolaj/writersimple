2b6104101a1503629661e1ea6ee24126385837bd
korbolajnennikolaj <141848103+korbolajnennikolaj@users.noreply.github.com>
2023-08-11 17:01:25 +0300

    Create example_v_1.5.0.py
    https://pypi.org/project/writersimple/

---
+++ example_v_1.5.0.py
@@ -0,0 +1,14 @@
+import writersimple
+
+print(writersimple.write("hello", 4, 5))
+print(writersimple.write_revers("hello", 4, 5))
+print(writersimple.write_combo("hello", 4, 5))
+print(writersimple.cipher("asdfghjklzxcvbnmqwertyuiop", "hello world"))
+print(writersimple.uncipher("asdfghjklzxcvbnmqwertyuiop", "5,18,8,8,24, 17,24,19,8,2,"))
+
+
+
+
+#    ||---------------------------------------
+#    \/                                       |
+#print(writersimple.writer_cool(1, 1))    # :) look at this 
